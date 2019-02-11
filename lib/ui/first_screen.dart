8e64310b27a4f5c7c5e7378cf708a544f985ffd8
apichart2425 <60070111@kmitl.ac.th>
2019-02-11 11:02:05 +0700

    textfield_img

--- lib/ui/first_screen.dart
+++ lib/ui/first_screen.dart
@@ -4,7 +4,7 @@ import './detailscreen.dart';
 class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Frist Screen"),
